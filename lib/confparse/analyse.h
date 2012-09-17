d62fd8555ec735b8dbb15eb4ed01f1e5a198c2b5
Matthieu Hautreux <matthieu.hautreux@cea.fr>
2012-09-17 10:19:49 +0200

    GPL: change confparse logic licensing to GPL

--- lib/confparse/analyse.h
+++ lib/confparse/analyse.h
@@ -1,3 +1,28 @@
+/*****************************************************************************\
+ *  lib/confparse/analyse.h - 
+ *****************************************************************************
+ *  Copyright  CEA/DAM/DIF (2005)
+ *
+ *  Written by : Philippe DENIEL   philippe.deniel@cea.fr
+ *               Thomas LEIBOVICI  thomas.leibovici@cea.fr
+ *
+ *  This file is part of Bridge, an abstraction layer to ease batch system and
+ *  resource manager usage in heterogeneous HPC environments.
+ *
+ *  Bridge is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Bridge is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Bridge.  If not, see <http://www.gnu.org/licenses/>
+\*****************************************************************************/
+
 /**
  *
  * \file    analyse.h
