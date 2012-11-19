3eeea3e4250deb34e44174cde23fcbd316dd4558
Matthieu Hautreux <matthieu.hautreux@cea.fr>
2012-11-19 11:39:03 +0100

    now use %R instead of %P in wrapped SLURM sinfo -o '...' command (ter)

--- scripts/batch_system/profiles/slurm.sh
+++ scripts/batch_system/profiles/slurm.sh
@@ -6,7 +6,7 @@ export BRIDGE_MSUB_STEPID=${SLURM_STEP_ID}
 export BRIDGE_MSUB_NNODE=${SLURM_NNODES:-1}
 export BRIDGE_MSUB_NPROC=${SLURM_NPROCS:-1}
 
-export BRIDGE_MSUB_QUEUE=$(squeue -o %P -h -j ${SLURM_JOB_ID})
+export BRIDGE_MSUB_QUEUE=$(squeue -o %R -h -j ${SLURM_JOB_ID})
 export BRIDGE_MSUB_PROJECT=$(squeue -o %a -h -j ${SLURM_JOB_ID})
 
 # not currently defined by Slurm in batch mode
